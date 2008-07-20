4e3cff1e75ebf19cac4ebe602cc76e5027ecf6e1
Remko Tronçon <git@el-tramo.be>
2008-07-20 16:49:00 +0200

    Documentation for the syntax.

--- themes/default/edit.php
+++ themes/default/edit.php
@@ -23,5 +23,34 @@
 			</form>
 		</div>
 
+		<div id="footer" style='text-align: left;'>
+			<p class='syntax'>
+				<span style='font-weight: bold;'>Syntax:</span> Besides normal HTML
+				code (e.g. <code>&lt;b&gt;Bold&lt;/b&gt;</code>), the following 
+				markup is available as well:
+				<ul>
+					<li><code>h1. Section</code>, <code>h2. Subsection</code>: 
+						Section headers
+					<li><code># Item</code>, <code>## Second-level item</code>:
+						Enumerated list</li>
+					<li><code>* Item</code>, <code>** Second-level item</code>:
+						Itemized list</li>
+					<li><code>"Some URL":http://someurl.com</code>: External links</li>
+					<li><code>!/path/to/image.jpg!</code>: Embedded images</li>
+					<li><code>_Emphasised text_, *strong text*, ??citations??, @code@,
+						+Inserted text+, -Removed text-</code> &rarr; 
+						<em>Emphasized text</em>, <strong>strong text</strong>,
+						<cite>citation</cite>, <ins>inserted text</ins>, <del>removed
+						text</del></li>
+					<li><code>H~2~O, A^2^</code> &rarr; H<sub>2</sub>O, A<sup>2</sup></li>
+					<li><code>Abbr(Abbreviation)</code> &rarr; <acronym tytle="Abbreviation">Abbr</acronym></li>
+					<li><code>|Cell 1|Cell 2|</code>: Tables</li>
+					<li><code>%{color:red}Red% text</code> &rarr; <span style='color:red'>Red</span> text</li>
+				</ul>
+				For more markup styles, see the 
+				<a href="http://hobix.com/textile/">Textile reference</a>.
+			</p>
+		</div>
+
 	</body>
 </html>
