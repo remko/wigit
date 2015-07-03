c74e670cf42b59bdf6a6aaa9c91806017b21abf7
Gerhard <gerhard@tinned-software.net>
2015-07-03 15:38:10 +0200

    extended the allowed characters in the page name.
    * The regex does not limit to Capital letter at the first character of the page name
    * The following characters are additionally  to "A-Za-z_" allowed in page name and link name "-."

--- index.php
+++ index.php
@@ -171,8 +171,8 @@
 		$text = preg_replace('@([^:"])(https?://([-\w\.]+)+(:\d+)?(/([%-\w/_\.]*(\?\S+)?)?)?)@', '$1<a href="$2">$2</a>', $text);
 
 		// WikiLinkify
-		$text = preg_replace('@\[([A-Z]\w+)\]@', '<a href="' . $SCRIPT_URL . '/$1">$1</a>', $text);
-		$text = preg_replace('@\[([A-Z]\w+)\|([\w\s]+)\]@', '<a href="' . $SCRIPT_URL . '/$1">$2</a>', $text);
+		$text = preg_replace('@\[([\w-\.]+)\]@', '<a href="' . $SCRIPT_URL . '/$1">$1</a>', $text);
+		$text = preg_replace('@\[([\w-\.]+)\|([\w-\.\s]+)\]@', '<a href="' . $SCRIPT_URL . '/$1">$2</a>', $text);
 
 		// Textilify
 		$textile = new Textile();
